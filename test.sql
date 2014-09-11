171bb4788bb605c425a8fe566afbfa12c855a8ee
robert <robertdinu@cronostechnologies.com>
2014-09-11 22:57:57 +0300

    replaced test.sql

--- test.sql
+++ test.sql
@@ -1,58 +1,105 @@
-     CREATE TABLE `users` (
-         `id` int(11) NOT NULL AUTO_INCREMENT,
-         `first_name` varchar(32) DEFAULT NULL,
-         `last_name` varchar(32) DEFAULT NULL,
-         `modified` datetime DEFAULT NULL,
-         PRIMARY KEY (`id`)
-     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 11, 2014 at 10:56 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
-     CREATE TABLE `points` (
-         `id` int(11) DEFAULT NULL,
-         `points` int(11) DEFAULT NULL
-     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-     CREATE TABLE `friend` (
-  `user` int(11) DEFAULT NULL,
-  `friend` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `live` tinyint(4) DEFAULT NULL,
-  `deleted` tinyint(4) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `live` (`live`),
-  KEY `deleted` (`deleted`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `message` (
-  `user` int(11) DEFAULT NULL,
-  `message` int(11) DEFAULT NULL,
-  `body` text DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `live` tinyint(4) DEFAULT NULL,
-  `deleted` tinyint(4) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `live` (`live`),
-  KEY `deleted` (`deleted`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-     CREATE TABLE `file` (
-       `id` int(11) NOT NULL AUTO_INCREMENT,
-       `name` varchar(255) DEFAULT NULL,
-       `mime` varchar(32) DEFAULT NULL,
-       `type` int(11) DEFAULT NULL,
-       `size` int(11) DEFAULT NULL,
-       `width` int(11) DEFAULT NULL,
-       `height` int(11) DEFAULT NULL,
-       `user` int(11) DEFAULT NULL,
-       `live` tinyint(4) DEFAULT NULL,
-       `deleted` tinyint(4) DEFAULT NULL,
-       `created` datetime DEFAULT NULL,
-       `modified` datetime DEFAULT NULL,
-       PRIMARY KEY (`id`),
-       KEY `live` (`live`),
-       KEY `deleted` (`deleted`)
+--
+-- Database: `library`
+--
 
-     ) ENGINE = InnoDB DEFAULT CHARSET = utf8 ;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE IF NOT EXISTS `author` (
+`id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`id`, `first_name`, `last_name`, `created`, `modified`) VALUES
+(1, 'Mark', 'Twain', '2014-09-11 17:33:28', NULL),
+(2, 'Bram', 'Stoker', '2014-09-11 17:33:28', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+`id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `author` int(11) NOT NULL,
+  `isbn` varchar(255) COLLATE utf8_bin NOT NULL,
+  `year` int(4) NOT NULL,
+  `rating` tinyint(2) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NULL DEFAULT NULL,
+  `deleted` datetime DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `title`, `author`, `isbn`, `year`, `rating`, `created`, `modified`, `deleted`) VALUES
+(1, 'Lorem Ipsom', 1, '978-3-16-148410-0', 2014, 2, '2014-09-11 13:12:28', NULL, NULL),
+(2, 'Contrary to popular belief, ', 2, '978-3-16-148410-1', 2014, 0, '2014-09-11 13:12:28', NULL, NULL),
+(3, 'There are many variations of passages of Lorem Ipsum available', 1, '978-3-16-148410-4', 2011, 2, '2014-09-11 13:13:52', NULL, NULL),
+(4, 'he standard chunk of Lorem Ipsum used since the 1500', 2, '978-3-16-148410-2', 2014, 0, '2014-09-11 13:13:52', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
